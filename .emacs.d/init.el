02c7edd79c37998f16ff91e1d2004d94928f2b72
kotorifan <kotorifan05@gmail.com>
2026-08-17 17:43:49 +0000

    Add files via upload

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -7,21 +7,37 @@
 ;; The theme I use
 ;;(use-package poet-theme
 ;;  :config (load-theme 'poet-dark t))
-
-(use-package almost-mono-themes
-  :config (load-theme 'almost-mono-black t))
+;;(use-package moe-theme
+;; :ensure t
+;; :config (load-theme 'moe-dark t))
+;; (use-package almost-mono-themes
+;;   :ensure t
+;;   :config (load-theme 'almost-mono-black t))
 ;; Languages
-(use-package forth-mode)
-(use-package markdown-mode)
-(use-package json-mode)
+(use-package zenburn-theme
+  :ensure t
+  :config
+  (load-theme 'zenburn t))
+(use-package markdown-mode
+  :ensure t)
+(use-package json-mode
+  :ensure t)
 (use-package sly
-  :init (setq inferior-lisp-program "clisp"))
+  :ensure t
+  :init (setq inferior-lisp-program "rlwrap sbcl"))
+(use-package forth-mode
+  :ensure t)
 (use-package cmake-mode
   :ensure t)
-
+(use-package web-mode
+  :mode "\\.php\\'"
+  :config
+  (setq web-mode-enable-auto-indentation t))
 ;; Development tools
-(use-package magit)
+(use-package magit
+  :ensure t)
 (use-package company
+  :ensure t
   :config (global-company-mode t))
 (use-package company-c-headers
   :ensure t
@@ -31,60 +47,9 @@
 (use-package format-all
   :ensure t
   :hook (c++-mode . format-all-mode))
-
-;; Other
-(use-package multiple-cursors
-  :bind ("C-S-c C-S-c" . mc/edit-lines))
-(use-package jinx
-  :hook (hook-mode . jinx-mode))
-
-(defun kotorifan/check-gnus ()
-"Generate a configuration for Gnus, if none exists"
-  (interactive)
-  (let ((auth-path (expand-file-name "~/.authinfo")))
-    (unless (file-exists-p auth-path)
-      (let ((username (read-string "Please enter the username for gnus: "))
-            (password (read-string "Please enter the password for gnus: ")))
-        (write-region
-         (format "machine news.eternal-september.org login %s password %s port 119\n"
-                 username password)
-         nil auth-path)))
-    (message "You may try opening gnus now")))
-
-(defun kotorifan/doxygen ()
-"Generate documentation with Doxygen"
-  (interactive)
-  (let ((default-directory (or (locate-dominating-file default-directory "Doxyfile")
-                               default-directory)))
-    (compile "doxygen Doxyfile")))
-
-(defun kotorifan/check-empty ()
-  (interactive)
-  (save-excursion
-    (goto-char (point-min))
-    (if (re-search-forward "^Subject: *$" nil t)
-        (error "The subject line is empty"))))
-
-(defun kotorifan/dl-file (&optional url download-dir download-name)
-  "Downloads a file to a given path, if no path is given, it goes to ~/Downloads"
-  (interactive)
-  (let* ((url (or url (read-string "Enter download URL: ")))
-         (dir (or download-dir "~/Downloads/"))
-         (name (or download-name (file-name-nondirectory url)))
-         (dest (expand-file-name name dir)))
-    (unless (file-exists-p dir) (make-directory dir t))
-    (url-copy-file url dest t)
-    (if (file-exists-p dest)
-        (message "The file was downloaded to %s" dest)
-      (message "Download failed"))))
-
-(defun kotorifan/check-server ()
-  "Checks whether Emacs runs as a daemon"
-  (interactive)
-  (if (daemonp)
-      (message "Emacs runs as a server")
-    (message "Emacs doesn't run as a server")))
-
+(use-package company-box
+  :ensure t
+  :hook (company-mode . company-box-mode))
 (setq c-default-style "linux"
       c-basic-offset 4)
 
@@ -104,12 +69,11 @@
       electric-indent-mode nil
       make-backup-files nil
       history-length 2000
-      whitespace-line-column 72
-      inferior-lisp-program "sbcl")
+      whitespace-line-column 72)
 
 (global-display-line-numbers-mode t)
 (global-hl-line-mode 1)
-
+(load "~/.quicklisp/clhs-use-local.el" 'noerror)
 (add-hook 'text-mode-hook 'turn-on-auto-fill)
 (add-hook 'mail-send-hook 'kotorifan/check-empty)
 (add-hook 'after-init-hook 'ruler-mode)
@@ -121,6 +85,7 @@
 
 (keymap-global-set "M-$" #'jinx-correct)
 (keymap-global-set "C-M-$" #'jinx-languages)
+ 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -129,17 +94,11 @@
  '(display-line-numbers-type 'relative)
  '(global-display-line-numbers-mode t)
  '(package-selected-packages
-   '(almost-mono-themes cmake-font-lock cmake-mode company
-                        company-c-headers company-doxygen doxymacs
-                        format-all forth-mode jinx json-mode magit
-                        markdown-mode modern-cpp-font-lock
-                        multiple-cursors poet-theme sly yasnippet
-                        yasnippet-snippets))
+   '(zenburn zenburn-theme web-mode php-mode company-box m68k-mode forth-mode almost-mono-themes nasm-mode ## sly moe-theme markdown-mode magit json-mode format-all company-c-headers cmake-mode))
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "IBM Plex Mono" :foundry "IBM " :slant normal :weight medium :height 120 :width normal)))))
-(put 'downcase-region 'disabled nil)
+ '(default ((t (:family "CozetteVector" :foundry "KBnP" :slant normal :weight medium :height 180 :width normal)))))
